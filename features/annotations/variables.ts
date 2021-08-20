dd4c3ac2a292eacde7da85b1ab2f61efe426b24e
Nils <nils@sld.gs>
2021-08-20 12:17:53 +0200

    add: syntax around variables, functions, objects, arrays, tuples, interfaces and classes

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -14,8 +14,8 @@ let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, false];
 
 // Classes
-class Car {}
-let car: Car = new Car();
+class Fish {}
+let fish: Fish = new Fish();
 
 // Object literal
 let point: { x: number; y: number } = { x: 10, y: 20 };
@@ -30,3 +30,25 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coords: { x: number; y: number } = JSON.parse(json);
 console.log(coords);
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'yellow'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) When we want a variable to have a type
+// that can't be inferred
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
---
+++ features/arrays.ts
@@ -0,0 +1,24 @@
+const carMakersEmpty: string[] = [];
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMakeEmpty: string[][] = [];
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const bestCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10']
+importantDates.push('2030-10-12');
+importantDates.push(new Date());
+
