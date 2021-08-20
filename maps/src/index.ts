120fae72bb60cb43ec858b5a666a369a6ec3d0c5
Nils <nils@sld.gs>
2021-08-20 13:31:41 +0200

    setup: google maps typescript example, creating private custom map, setup fake data

---
+++ maps/src/index.ts
@@ -0,0 +1,12 @@
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+new CustomMap('map');
+
+// const user = new User();
+// console.log(user);
+
+// const company = new Company();
+// console.log(company);
