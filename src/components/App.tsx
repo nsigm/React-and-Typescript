e93b29d41420399ed089c448f2e28d76474b2da0
Nils <n.sigmund@outlook.com>
2021-08-23 13:45:05 +0200

    add: react side, wiring up redux, fixing initial state of reduxer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,15 @@
+import { Provider } from 'react-redux';
+import { store } from '../store';
+import RepositoriesList from './RepositoriesList';
 const App = () => {
-  return
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a Package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
