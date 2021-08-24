73f0a5db1fae2bedff1ec1e2a5f456df7f2e046c
Nils <n.sigmund@outlook.com>
2021-08-24 16:20:27 +0200

    add: refactor into multiple plugins, workaround css import with esbuild using regex, escaping css symbols that break js workaround

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -27,13 +28,13 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
-      }
+      },
     });
-    
+
     setCode(result.outputFiles[0].text);
   };
 
