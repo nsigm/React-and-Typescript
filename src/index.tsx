42f200b1013214668eedb2dd63c3740736c64dd3
Nils <n.sigmund@outlook.com>
2021-08-26 17:05:58 +0200

    add: code editor component using monaco, configure and type defs, handle editor change events, add code formatting using prettier and syntax highlighting, fix type def

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -62,6 +64,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue="const a = b;" onChange={(value) => setInput(value)}/>
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Submit</button>
