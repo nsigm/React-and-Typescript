00c34058eb54f0b8b5fb92302efbf9ae712fce5f
Nils <n.sigmund@outlook.com>
2021-08-26 21:42:56 +0200

    fix: error handling for async functions, build errors in preview

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
 const html = `
@@ -13,13 +14,20 @@ const html = `
   <body style="height: 100px">
     <div id="root"></div>
     <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + err + '</div>'
+        console.error(err);
+      }
+      window.addEventListener('error', (event) =>{
+        event.preventDefault();
+        handleError(event.error);
+      })
       window.addEventListener('message', (event) => {
         try {
           eval(event.data);
         } catch (err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + err + '</div>'
-          console.error(err);
+          handleError(err);
         }
       }, false);
     </script>
@@ -27,7 +35,7 @@ const html = `
 </html>
 `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -40,6 +48,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
     }, 100);
   };
 
+  console.log(err)
   return (
     <div className="preview-wrapper">
       <iframe
@@ -49,6 +58,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         srcDoc={html}
         onLoad={loadHandler}
       />
+      {err && <div className="preview-error">{err}</div>}
     </div>
   );
 };
