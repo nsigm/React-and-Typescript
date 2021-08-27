a14b038cebd54f72a3e49b1d4cace1e32f3036b6
Nils <n.sigmund@outlook.com>
2021-08-27 15:26:00 +0200

    add: bind react with redux, create cell list and cell item, apply useDispatch via custom hook, start connecting codecell and texteditor to our store

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
-        <CodeCell />
+        <CellList />
       </div>
     </Provider>
   );
